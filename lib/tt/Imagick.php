e43231c2d6ffd072728e272d9be3e016f62ea8b3
tokushima <tokushima@192.168.0.119>
2019-09-11 00:02:35 +0900

    init

---
+++ lib/tt/Imagick.php
@@ -0,0 +1,209 @@
+<?php
+namespace tt;
+
+class Imagick{
+	/**
+	 * 縦向き
+	 * @var integer
+	 */
+	const ORIENTATION_PORTRAIT = 1;
+	/**
+	 * 横向き
+	 * @var integer
+	 */
+	const ORIENTATION_LANDSCAPE = 2;
+	/**
+	 * 正方形
+	 * @var integer
+	 */
+	const ORIENTATION_SQUARE = 3;
+	
+	private $canvas;
+	
+	public function __construct($filename){
+		if($filename != __FILE__){
+			$this->canvas = new \Imagick($filename);
+		}
+	}
+	public function __destruct(){
+		$this->canvas->clear();
+	}
+	
+	/**
+	 * バイナリ文字列から画像を読み込む
+	 * @param string $string
+	 * @return \tt\Imagick
+	 */
+	public static function readImageBlob($string){
+		$self = new static(__FILE__);
+		$self->canvas = new \Imagick();
+		
+		return $self;
+	}
+	
+	/**
+	 * 塗りつぶした矩形を作成する
+	 * @param integer $width
+	 * @param integer $height
+	 * @param string $color
+	 * @param string $filename
+	 * @return \tt\Imagick
+	 */
+	public static function filled_rectangle($width,$height,$color){
+		$self = new static(__FILE__);
+		$self->canvas = new \Imagick();
+		$self->canvas->newImage($width,$height,$color);
+		
+		return $self;
+	}
+	
+	/**
+	 * ファイルに書き出す
+	 * @param string $filename
+	 */
+	public function write($filename){
+		$this->canvas->writeImage($filename);
+	}
+	
+	/**
+	 * 画像を出力する
+	 * @param string $format
+	 */
+	public function output($format='jpeg'){
+		$format = strtolower($format);
+		
+		switch($format){
+			case 'png':
+				header('Content-Type: image/png');
+				break;
+			case 'gif':
+				header('Content-Type: image/gif');
+				break;
+			default:
+				header('Content-Type: image/jpeg');
+				$format = 'jpeg';
+		}
+		$this->canvas->setImageFormat($format);
+		print($this->canvas);
+	}
+	
+	
+	/**
+	 * 画像の一部を抽出する
+	 * @param integer $width 抽出する幅
+	 * @param integer $height 抽出する高さ
+	 * @param integer $x 抽出する領域の左上の X 座標
+	 * @param integer $y 抽出する領域の左上の Y 座標
+	 * @return \tt\Imagick
+	 */
+	public function crop($width,$height,$x=null,$y=null){
+		list($w,$h) = $this->get_size();
+		
+		if($width >= $w && $height >= $h){
+			return $this;
+		}
+		
+		if($x === null || $y === null){
+			$x = ($w - $width) / 2;
+			$y = ($h - $height) / 2;
+			
+			list($x,$y) = [($x >= 0) ? $x : 0,($y >= 0) ? $y : 0];
+		}
+		if($x < 0){
+			$x = $w + $x;
+		}
+		if($y < 0){
+			$y = $h + $y;
+		}
+		$this->canvas->cropImage($width,$height,$x,$y);
+		
+		return $this;
+	}
+	
+	/**
+	 * 画像のサイズを変更する
+	 * @param integer $width 変更後の幅
+	 * @param integer $height 変更後の高さ
+	 * @return \tt\Imagick
+	 */
+	public function resize($width,$height=null){
+		list($w,$h) = $this->get_size();
+		$rw = empty($width) ? 1 : $width;
+		$rh = empty($height) ? 1 : $height;
+		
+		if(!empty($width) && !empty($height)){
+			$aw = $rw / $w;
+			$ah = $rh / $h;
+			$a = max($aw,$ah);
+		}else if(!isset($height)){
+			$a = $rw / $w;
+		}else{
+			$a = $rh / $h;
+		}
+		$cw = $w * $a;
+		$ch = $h * $a;
+		
+		$this->canvas->scaleImage($cw,$ch);
+		
+		return $this;
+	}
+	
+	/**
+	 * 回転
+	 * @param integer $angle 角度
+	 * @param string $background_color
+	 * @return \tt\Imagick
+	 */
+	public function rotate($angle,$background_color='#000000'){
+		$this->canvas->rotateImage($background_color,$angle);
+		
+		return $this;
+	}
+	
+	/**
+	 * マージ
+	 * @param integer $x
+	 * @param integer $y
+	 * @param \tt\Imagick $img
+	 * @return \tt\Imagick
+	 */
+	public function merge($x,$y,\tt\Imagick $img){
+		$this->canvas->compositeImage(
+			$img->canvas,
+			\Imagick::COMPOSITE_OVER,
+			$x,
+			$y
+		);
+		return $this;
+	}
+	
+	
+	/**
+	 * サイズ
+	 * @return integer[]
+	 */
+	public function get_size(){
+		$w = $this->canvas->getImageWidth();
+		$h = $this->canvas->getImageHeight();
+		
+		return [$w,$h];
+	}
+	
+	/**
+	 * 画像の向き
+	 * @return  integer
+	 */
+	public function get_orientation(){
+		list($w,$h) = $this->get_size();
+		
+		$d = $h / $w;
+		
+		if($d <= 1.02 && $d >= 0.98){
+			return self::ORIENTATION_SQUARE;
+		}else if($d > 1){
+			return self::ORIENTATION_PORTRAIT;
+		}else{
+			return self::ORIENTATION_LANDSCAPE;
+		}
+	}
+}
